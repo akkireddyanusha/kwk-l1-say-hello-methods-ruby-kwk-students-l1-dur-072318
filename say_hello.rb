5e38184e8609aca8eeb3957ea47d8b523335d158
akkireddyanusha <akkireddyanusha@gmail.com>
2018-07-24 14:29:44 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,6 +1,6 @@
 # Build your say_hello method here
-def say_hello (your_name)
+def say_hello (your_name="Anusha")
   puts "Hello #{your_name}!"
 end
 
-say_hello ("Gabriela")
+say_hello ("Karlie Kloss")
